4e125cf59cdc6199fccea6296ee33bef1eb48f0f
mika <imikado@gmail.com>
2024-11-24 17:44:30 +0100

    update category view

--- lib/Views/category_view.dart
+++ lib/Views/category_view.dart
@@ -22,9 +22,9 @@ class CategoryView extends StatefulWidget {
 
 enum AppDisplay { list, grid }
 
-const List<(AppDisplay, String)> appDisplayOptions = <(AppDisplay, String)>[
-  (AppDisplay.list, 'list'),
-  (AppDisplay.grid, 'grid'),
+const List<(AppDisplay, IconData)> appDisplayOptions = <(AppDisplay, IconData)>[
+  (AppDisplay.list, Icons.view_list),
+  (AppDisplay.grid, Icons.view_compact),
 ];
 
 class _CategoryViewState extends State<CategoryView> {
@@ -73,34 +73,39 @@ class _CategoryViewState extends State<CategoryView> {
         controller: scrollController,
         child: Column(
           children: [
-            Row(children: [
-              Expanded(child: SizedBox()),
-              SegmentedButton<AppDisplay>(
-                // ToggleButtons above allows multiple or no selection.
-                // Set `multiSelectionEnabled` and `emptySelectionAllowed` to true
-                // to match the behavior of ToggleButtons.
-                multiSelectionEnabled: false,
-                emptySelectionAllowed: false,
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(children: [
+                  Expanded(child: SizedBox()),
+                  SegmentedButton<AppDisplay>(
+                    // ToggleButtons above allows multiple or no selection.
+                    // Set `multiSelectionEnabled` and `emptySelectionAllowed` to true
+                    // to match the behavior of ToggleButtons.
+                    multiSelectionEnabled: false,
+                    emptySelectionAllowed: false,
 
-                // Hide the selected icon to match the behavior of ToggleButtons.
-                showSelectedIcon: true,
-                // SegmentedButton uses a Set<T> to track its selection state.
-                selected: _segmentedButtonSelection,
-                // This callback updates the set of selected segment values.
-                onSelectionChanged: (Set<AppDisplay> newSelection) {
-                  setState(() {
-                    _segmentedButtonSelection = newSelection;
-                  });
-                },
-                // SegmentedButton uses a List<ButtonSegment<T>> to build its children
-                // instead of a List<Widget> like ToggleButtons.
-                segments: appDisplayOptions.map<ButtonSegment<AppDisplay>>(
-                    ((AppDisplay, String) shirt) {
-                  return ButtonSegment<AppDisplay>(
-                      value: shirt.$1, label: Text(shirt.$2));
-                }).toList(),
-              ),
-            ]),
+                    // Hide the selected icon to match the behavior of ToggleButtons.
+                    showSelectedIcon: true,
+                    // SegmentedButton uses a Set<T> to track its selection state.
+                    selected: _segmentedButtonSelection,
+                    // This callback updates the set of selected segment values.
+                    onSelectionChanged: (Set<AppDisplay> newSelection) {
+                      setState(() {
+                        _segmentedButtonSelection = newSelection;
+                      });
+                    },
+                    // SegmentedButton uses a List<ButtonSegment<T>> to build its children
+                    // instead of a List<Widget> like ToggleButtons.
+                    segments: appDisplayOptions.map<ButtonSegment<AppDisplay>>(
+                        ((AppDisplay, IconData) shirt) {
+                      return ButtonSegment<AppDisplay>(
+                          value: shirt.$1, label: Icon(shirt.$2));
+                    }).toList(),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ])),
             Expanded(
               child: _segmentedButtonSelection.first == AppDisplay.grid
                   ? getGridView()
